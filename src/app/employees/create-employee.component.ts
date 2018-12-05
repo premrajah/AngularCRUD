28afb34b3ceba87f220a3c25724faf9291cad3c1
Prem Rajah <prajah@ctlondon.com>
2018-12-05 12:51:35 +0000

    added theme to ngx-bootstrap datepicker

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
 
+// ngx-bootstrap
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
 
 @Component({
   selector: 'app-create-employee',
@@ -14,15 +17,36 @@ export class CreateEmployeeComponent implements OnInit {
   // isActive = true; // lets the checkbox button be checked/unchecked
   // department = 2; // lets the select list be selected
 
+  bsConfig: Partial<BsDatepickerConfig>;
+  colorTheme = 'theme-dark-blue';
+  dateType = 'DD-MM-YYYY';
+  
+  // default date
+  dateOfBirth: Date = new Date(1981, 4, 31);
+
   departments: Department[] = [
-    { id: 1, name: 'Help Desk'},
-    { id: 2, name: 'HR'},
-    { id: 3, name: 'IT'},
-    { id: 4, name: 'Payroll'},
-    { id: 5, name: 'Admin '}
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' },
+    { id: 5, name: 'Admin ' }
   ];
 
-  constructor() { }
+  constructor() {
+
+
+    // useful for copying properties
+    // ngx-bootstrap config properties
+    this.bsConfig = Object.assign({}, 
+      { 
+        containerClass: this.colorTheme, 
+        showWeekNumbers: false,
+        minDate: new Date(1914, 0 , 1),
+        maxDate: new Date(),
+        dateInputFormat: this.dateType
+        
+      });
+  }
 
   ngOnInit() {
   }
