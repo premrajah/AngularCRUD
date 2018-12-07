fe35454cda16caf0c03586a910250c858e4caf36
Prem Rajah <prajah@ctlondon.com>
2018-12-07 15:36:24 +0000

    finish prop detection with setter, start setter vs ngOnChanges

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 
 import { Employee } from '../models/employee.model';
@@ -8,23 +8,26 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit, OnChanges {
+export class DisplayEmployeeComponent implements OnInit {
 
-  @Input() employee: Employee;
+  // backing field for Input property
+  private _employee: Employee;
+
+  @Input()
+  set employee(val: Employee) {
+    console.log(this._employee);
+    console.log('Previous: ' + (this._employee ? this._employee.name : 'NULL'));
+    console.log('Current: ' + val.name)
+    this._employee = val;
+  }
+  get employee(): Employee {
+    return this._employee;
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-
-    // console.log(changes);
-    const previousEmployee = <Employee>changes.employee.previousValue;
-    const currentEmployee = <Employee>changes.employee.currentValue;
-
-    console.log('Previous: ' + (previousEmployee ? previousEmployee.name : 'NULL'));
-    console.log('Current: ' + currentEmployee.name);
-  }
 
 }
